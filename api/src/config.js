7f4c3c39821e168dc414a0ab9ee9a94c0b58987d
arnaudambro <arnaud.ambroselli@yahoo.fr>
2020-08-24 20:25:06 +0200

    remove api from folder

--- api/src/config.js
+++
@@ -1,6 +0,0 @@
-const MONGO_URL = process.env.DB_ACCOUNT;
-const PORT = process.env.PORT || 3000;
-const SECRET = process.env.SECRET || "not_so_secret";
-const ENVIRONMENT = process.env.NODE_ENV;
-
-module.exports = { PORT, MONGO_URL, ENVIRONMENT, SECRET };
